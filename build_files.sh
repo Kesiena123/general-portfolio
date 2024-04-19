f8cbb92cfd04e2d4d3e8a38bd556f4cd89c29782
Kesiena123 <biagodstimei@gmail.com>
2024-04-19 10:24:38 -0700

    update3

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-py -m pip install -r requirements.txt
-python3.9 manage.py collectstatic
+pip install -r requirements.txt
+python manage.py collectstatic
+python manage.py migrate
+python manage.py runserver
